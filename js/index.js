c51662ead0b22cc29233fa2cb5494d157e5e9c01
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-02-22 00:16:42 -0600

    Added services page navigation tabs components

--- js/index.js
+++ js/index.js
@@ -32,4 +32,38 @@ navLink.addEventListener('click', event => {
     hamburgerClosed.style.display = "none"
 })
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
- 
+ //Services component~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class TabLink {
+    constructor(element) {
+      this.element = element
+      this.data = this.element.dataset.tab
+      this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+      this.tabItem = new TabItem(this.itemElement);
+      this.element.addEventListener('click',this.select.bind(this))
+      this.element.addEventListener('click', e => console.log(' a tab has been clicked'))
+    }
+  
+    select() {
+      const links = document.querySelectorAll('.tabs-link')
+      links.forEach(link => link.classList.remove('tabs-link-selected'));
+      this.element.classList.add('tabs-link-selected')
+      this.tabItem.select()
+    }
+  }
+  
+class TabItem {
+    constructor(element) {
+      this.element = element
+    }
+  
+    select() {
+    const items = document.querySelectorAll('.tabs-item')
+      items.forEach(element => element.classList.remove("tabs-item-selected"))
+      this.element.classList.add('tabs-item-selected')
+    }
+  }
+
+let links = document.querySelectorAll('.tabs-link');
+links.forEach(link => new TabLink(link))
