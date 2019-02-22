470b766aa0f3fcdb98607e8297a76b46d692637c
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-02-21 19:24:14 -0600

    Added hamburger menu expanded overlay for desktop and mobile versions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-// JS goes here
+// ~~~~~~~~~~~~~~~~~~~~~~Menu Bar~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+
+const hamburgerOpen = document.querySelector('.hamburger-open')
+const hamburgerClosed = document.querySelector('.hamburger-closed')
+const menuOptions = document.querySelector('.menu-options')
+const navLink = document.querySelector('.home')
+
+
+hamburgerOpen.addEventListener('click', event => {
+    console.log('click is working')
+    hamburgerClosed.style.display = "initial"
+    hamburgerOpen.style.display = "none"
+    menuOptions.style.visibility = "visible"
+})
+
+hamburgerClosed.addEventListener('click', event => {
+    console.log('click is working')
+    hamburgerClosed.style.display = "none"
+    hamburgerOpen.style.display = "initial"
+    menuOptions.style.visibility = "hidden"
+})
+
+navLink.addEventListener('click', event => {
+    console.log('click work')
+    menuOptions.style.visibility = "hidden"
+    hamburgerOpen.style.display = "initial"
+    hamburgerClosed.style.display = "none"
+})
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+ 
